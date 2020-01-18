39d35cca0bf61eb61f339823e24803c0adb9283f
Robin Laugs <robinlaugs@live.nl>
2020-01-18 11:32:51 +0100

    Minor improvements

--- FizzBuzz.java
+++ FizzBuzz.java
@@ -1,13 +1,13 @@
-
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class FizzBuzz {
 
-    private static final String OUTPUT_SEPARATOR = ", ";
     private static final int NUMBER_COUNT = 25;
+    private static final String OUTPUT_SEPARATOR = ", ";
 
     private static final Map<Integer, String> CONVERSIONS = new HashMap<>();
 
@@ -17,20 +17,22 @@ public class FizzBuzz {
     }
 
     public static void main(String[] args) {
-        IntStream numbers = IntStream.rangeClosed(1, NUMBER_COUNT);
-
-        numbers.mapToObj(FizzBuzz::convert)
-                .map(output -> output + OUTPUT_SEPARATOR)
-                .forEach(System.out::print);
+        IntStream.rangeClosed(1, NUMBER_COUNT)
+                .mapToObj(FizzBuzz::convert)
+                .forEach(FizzBuzz::print);
     }
 
     private static String convert(int number) {
         String converted = CONVERSIONS.entrySet().stream()
                 .filter(entry -> number % entry.getKey() == 0)
-                .map(Map.Entry::getValue)
+                .map(Entry::getValue)
                 .collect(Collectors.joining());
 
         return converted.isEmpty() ? String.valueOf(number) : converted;
+    }
+
+    private static void print(String value) {
+        System.out.print(value + OUTPUT_SEPARATOR);
     }
 
 }
