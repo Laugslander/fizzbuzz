27dfd93459cd054b49715066285996bb7f4aa0c0
Robin Laugs <robinlaugs@live.nl>
2020-03-23 16:31:37 +0100

    Minor improvements

--- FizzBuzz.java
+++ FizzBuzz.java
@@ -1,34 +1,31 @@
 import java.util.Map;
 import java.util.Map.Entry;
-import java.util.TreeMap;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class FizzBuzz {
 
-    private static final int NUMBER_COUNT = 25;
+    private static final int SEQUENCE_LENGTH = 25;
     private static final String OUTPUT_SEPARATOR = ", ";
 
-    private static final Map<Integer, String> CONVERSIONS = new TreeMap<>();
-
-    static {
-        CONVERSIONS.put(3, "Fizz");
-        CONVERSIONS.put(5, "Buzz");
-    }
+    private static final Map<Integer, String> CONVERSIONS = Map.of(
+            3, "Fizz",
+            5, "Buzz"
+    );
 
     public static void main(String[] args) {
-        IntStream.rangeClosed(1, NUMBER_COUNT)
+        IntStream.rangeClosed(1, SEQUENCE_LENGTH)
                 .mapToObj(FizzBuzz::convert)
                 .forEach(FizzBuzz::print);
     }
 
-    private static String convert(int number) {
-        String converted = CONVERSIONS.entrySet().stream()
+    private static String convert(final int number) {
+        String conversion = CONVERSIONS.entrySet().stream()
                 .filter(entry -> number % entry.getKey() == 0)
                 .map(Entry::getValue)
                 .collect(Collectors.joining());
 
-        return converted.isEmpty() ? String.valueOf(number) : converted;
+        return conversion.isEmpty() ? String.valueOf(number) : conversion;
     }
 
     private static void print(String value) {
