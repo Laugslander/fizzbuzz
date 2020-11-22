834be5c84e7045cfdf10dd00a9c254b3a2d95b91
Robin Laugs <robinlaugs@live.nl>
2020-11-22 19:50:23 +0100

    Final improvements for Java 11

--- FizzBuzz.java
+++ FizzBuzz.java
@@ -1,31 +1,29 @@
 import java.util.Map;
 import java.util.Map.Entry;
-import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class FizzBuzz {
 
-    private static final int SEQUENCE_LENGTH = 25;
+    private static final int RANGE_LENGTH = 30;
     private static final String OUTPUT_SEPARATOR = ", ";
 
-    private static final Map<Integer, String> CONVERSIONS = Map.of(
-            3, "Fizz",
-            5, "Buzz"
+    private static final Map<Integer, String> CONVERSIONS = Map.ofEntries(
+            Map.entry(3, "Fizz"),
+            Map.entry(5, "Buzz")
     );
 
     public static void main(String[] args) {
-        IntStream.rangeClosed(1, SEQUENCE_LENGTH)
+        IntStream.rangeClosed(1, RANGE_LENGTH)
                 .mapToObj(FizzBuzz::convert)
                 .forEach(FizzBuzz::print);
     }
 
-    private static String convert(final int number) {
-        String conversion = CONVERSIONS.entrySet().stream()
-                .filter(entry -> number % entry.getKey() == 0)
+    private static String convert(int number) {
+        return CONVERSIONS.entrySet().stream()
+                .filter(e -> number % e.getKey() == 0)
                 .map(Entry::getValue)
-                .collect(Collectors.joining());
-
-        return conversion.isEmpty() ? String.valueOf(number) : conversion;
+                .reduce(String::concat)
+                .orElse(String.valueOf(number));
     }
 
     private static void print(String value) {
